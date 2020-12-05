9cfbad2c8d00a47357e0413d82b207e8de19efb4
Nirmalie <wnirmalie@yahoo.com>
2020-12-05 15:59:45 +0100

    created a new site

---
+++ script.js
@@ -0,0 +1,37 @@
+$(function() { 
+    
+    $('#hamburger-container').on('click', function() {
+
+    $('#sideMenu').toggle('slide', { direction: 'right'}, 1000)
+    });
+  var listaImmagini = './img/back.jpg'
+    $('#style').each(function(i, el) {
+    $(el).parallax({imageSrc: listaImmagini})
+   });
+
+   $(window).on('scroll', function() {
+      var scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+        if (scrollPosition > 700) {
+          console.log('maggiore di 700')
+        $('#tornaSu').fadeIn();
+        } else {
+            $('#tornaSu').fadeOut();
+        }
+    })
+   
+    $('#tornaSu').on('click', function() {
+     $('html, body').animate({scrollTop: 0}, 5500)
+    })
+
+     $('header ul li a').on('click', function(event) {
+      event.preventDefault();
+     $('html, body').animate({scrollTop: $(this.hash).offset().top}, 1500)
+     $('#sideMenu').toggle('slide', { direction: 'right'}, 1000)
+    })
+
+    $(document).ready(function(e){
+    $('.fancybox').fancybox({autoPlay:true, playSpeed:2000});
+     })
+  })
+
+ 
