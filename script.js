63a315d70567babfef23b354e43b000af604d78b
Nirmalie <61603665+nirmalieo3@users.noreply.github.com>
2020-12-06 09:13:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ $(function() {
 
     $('#sideMenu').toggle('slide', { direction: 'right'}, 1000)
     });
-  var listaImmagini = './img/back.jpg'
-    $('#style').each(function(i, el) {
+  var immagini = './img/back.jpg'
+   /* $('#style').each(function(i, el) {
     $(el).parallax({imageSrc: listaImmagini})
-   });
+   });*/
+    $('#style').paralax({imageSrc: immagini});
 
    $(window).on('scroll', function() {
       var scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
